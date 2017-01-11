4f16a229da764aa654d66c6de8e466c2c1c35e86
Michael Neuder <michael.neuder@gmail.com>
2017-01-11 15:03:14 -0700

    cleaning stuff up

--- cube.hpp
+++ cube.hpp
@@ -7,13 +7,13 @@ struct face
 	//a string for each of the nine positiions on the face, the string will hold the color
 };
 
-struct edge
-{
+struct edge//edge pieces are between corners that have two colors
+{//each cube has twelve edges
 	std::string color1, color2;
 };
 
-struct corner
-{
+struct corner//the intersection of three faces
+{//8 corners per cube
 	std::string color1, color2, color3;
 };
 
@@ -26,42 +26,42 @@ class cube
 		face bottom_face;
 		face left_face;
 		face right_face;
+		
 		edge edgeF1, edgeF2, edgeF3, edgeF4;//these will hold data on the edges
 		edge edgeM1, edgeM2, edgeM3, edgeM4;
 		edge edgeBa1, edgeBa2, edgeBa3, edgeBa4;
+		
 		corner cornerF1, cornerF2, cornerF3, cornerF4;//corner data
 		corner cornerB1, cornerB2, cornerB3, cornerB4;
-		cube();
-		void listFaces();
-		void setCube();
-		void defaultCube();
-		//edge-corner stuff
-		void updateEdges();
-		void updateCorners();
+		
+		cube();//constructor for cude
+		void listFaces();//prints current state of face
+		void setCube();//allows custom input of data for cube position
+		void defaultCube();//default = solved
+		
+		//edge and corner methods
+		void updateEdges();//called to reassign edge properties after rotations
+		void updateCorners();//called to reassign corner properties after rotations
 		void printEdges();
 		void printCorners();
-		std::string findEdge(std::string _color1, std::string _color2);
-		std::string findCorner(std::string _color1, std::string _color2, std::string _color3);
-		void firstEdge(std::string location);
-		void secondEdge(std::string location);
-		void thirdEdge(std::string location);
-		void fourthEdge(std::string location);
-		void corner(std::string location);
-		void insert(std::string location);
-		void topCross();
-		void alignCross();
-		void sortCorners();
-		void finishCube();
-	
-
+		std::string findEdge(std::string _color1, std::string _color2);//finds edge position
+		std::string findCorner(std::string _color1, std::string _color2, std::string _color3);//finds corner position
 		
-		void solveCube();
+		//cube solving functions
+		void firstEdge(std::string location);//these are solving the first four edges
+		void secondEdge(std::string location);//could be written in one function
+		void thirdEdge(std::string location);//chose to write four separate because it is less unecessary rotations for user
+		void fourthEdge(std::string location);
+		void corner(std::string location);//solves first four corners
+		void insert(std::string location);//insert edge to left or right
+		void topCross();//creates a cross on top face
+		void alignCross();//orders the cross edge pieces
+		void sortCorners();//sorts the final four corners
+		void finishCube();//completes the cube by solving four final corners
+	
+		void solveCube();//essentially the main that calls all the cube solving functions based on current cube state
 		
 		//algorithms
-		void solveCorners();
-		void leftInsert();
-		void rightInsert();
-		void rotate(face new_front, face new_top);
 		void rotateUp();
 		void rotateLeft();
 		
